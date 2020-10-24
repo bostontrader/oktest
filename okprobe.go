652667bd923dcb09e6df07977e80e30ba273b3c0
Thomas Radloff <bostontrader@gmail.com>
2020-10-24 09:46:09 +0800

    More okprobe tests.

--- okprobe.go
+++ okprobe.go
@@ -8,23 +8,22 @@ import (
 
 /* Give the baseURL of the okex or okcatbox server, an OKProbe command, and file names containing the server credentials for read, read-trade, and read-withdraw, run a series of tests of the given OKProbe command.
  */
-func testOKProbe(baseURL, command, read, trade, withdraw string) {
+func testOKProbe(baseURL, command, queryString, read, trade, withdraw string) {
 
 	test([]string{command, "--baseURL", baseURL, "--credentialsFile", read, "--makeErrorsCredentials", "--makeErrorsParams"})
 	test([]string{command, "--baseURL", baseURL, "--credentialsFile", read, "--makeErrorsWrongCredentialsType"})
 	test([]string{command, "--baseURL", baseURL, "--credentialsFile", trade, "--makeErrorsWrongCredentialsType"})
 	test([]string{command, "--baseURL", baseURL, "--credentialsFile", withdraw, "--makeErrorsWrongCredentialsType"})
-	test([]string{command, "--baseURL", baseURL, "--credentialsFile", read, "--forReal"})
+	test([]string{command, "--baseURL", baseURL, "--credentialsFile", read, "--queryString", queryString, "--forReal"})
 	fmt.Printf("test okprobe %s success\n", command)
 }
 
 func test(args []string) {
 
-	_, err := exec.Command("okprobe", args...).Output()
+	cmd := exec.Command("okprobe", args...)
+	out, err := cmd.Output()
 	if err != nil {
-		fmt.Printf("okprobe error %v\n", err)
+		fmt.Printf("okprobe: out=%s, err=%v\nargs=%v\n", string(out), err, args)
 		os.Exit(1)
-	} else {
-		//fmt.Printf("okprobe success %v\n", out)
 	}
 }
